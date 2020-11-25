d4b5c84f0e0917cb00735ebc44188f1c41ca1ca9
Bosco Choi <bchoi@lcs.on.ca>
2020-11-25 09:57:35 -0500

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Chun Choi on 2020-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Chun Choi on 2020-11-25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
