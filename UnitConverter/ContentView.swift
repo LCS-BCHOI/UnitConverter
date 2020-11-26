a8d658c797edfe47584102c10b40f8c78f444f15
Bosco Choi <bchoi@lcs.on.ca>
2020-11-26 08:56:00 -0500

    Finished The Title Column

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        return self.modifier(Title())
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
